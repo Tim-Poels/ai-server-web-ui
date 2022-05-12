f73d15c54eda015364f6e21beaf92e05877bad39
Tim-Poels <timpoels2000@gmail.com>
2022-05-12 16:08:49 +0200

    navbar redesign with green dots

--- src/components/Training/Training.js
+++ src/components/Training/Training.js
@@ -1,57 +1,65 @@
-import React from 'react';
-import './training.css';
-import TrainingInput from './TrainingInput.js';
-import styled from 'styled-components';
+import React, { useEffect } from "react";
+import "./training.css";
+import TrainingInput from "./TrainingInput.js";
+import styled from "styled-components";
+import colorNav from "./ColorNav.js";
 
-
-function Training({ title }) {
-  return (
-    <Wrapper>
-      <BoxTitle>{title}</BoxTitle>
-      <InputCont>
-        <TrainingInput title={"DockerHub image slug"} placeholder={"DockerHub_username/image_name"} />
-        <TrainingInput title={"Volume"} placeholder={"/output"} />
-        <ButtonCont>
-          <button className='submitButton'>Submit</button>
-        </ButtonCont>
-      </InputCont>
-    </Wrapper>
-  );
+function Training() {
+	useEffect(() => {
+		colorNav("tab1")
+	});
+	return (
+		<Wrapper>
+			<BoxTitle>Training Launcher</BoxTitle>
+			<InputCont>
+				<TrainingInput
+					title={"DockerHub image slug"}
+					placeholder={"DockerHub_username/image_name"}
+				/>
+				<TrainingInput title={"Volume"} placeholder={"/output"} />
+				<ButtonCont>
+					<button className="submitButton">Submit</button>
+				</ButtonCont>
+			</InputCont>
+		</Wrapper>
+	);
 }
 
-export default Training
-
-
+export default Training;
 
 const Wrapper = styled.div`
-  display: flex;
-  flex-direction: column;
-  height: 55vh;
-  width: 40vw;
-  box-shadow: 15px 15px 50px rgba(0, 0, 0, 0.25);
-  padding: 30px;
+	display: flex;
+	flex-direction: column;
+	height: 110%;
+	margin-left: 50px;
+	width: 100%;
+	box-shadow: 15px 15px 50px rgba(0, 0, 0, 0.25);
+	padding: 20px;
+	padding-left: 40px;
+	padding-right: 30px;
 `;
 
 const BoxTitle = styled.h2`
-  margin-left: auto;
-  margin-right: auto;
-  font-family: 'poppins', sans-serif;
-  font-style: normal;
-  font-size: 36px;
-  font-weight: 600;
-  line-height: 28px;
-  `;
+	height: 20%;
+	width: 100%;
+	color: black;
+	font-family: "poppins", sans-serif;
+	font-style: normal;
+	font-size: 36px;
+	font-weight: 600;
+	text-align: center;
+`;
 
 const InputCont = styled.div`
-  display:flex;
-  flex-direction: column;
-  height: 500px;
-  justify-content: space-between;
-  margin-top: 80px;
-  `;
+	display: flex;
+	flex-direction: column;
+	height: 80%;
+  width: 75%;
+`;
 
 const ButtonCont = styled.div`
-  height: 80px;
-  width: 140px;
-  align-self: flex-end;
-`
+	height: 17.5%;
+	width: 35%;
+	align-self: flex-end;
+	margin-top: 25px;
+`;
--- src/components/Training/TrainingInput.js
+++ src/components/Training/TrainingInput.js
@@ -14,7 +14,7 @@ function TrainingInput({ title, placeholder }) {
   }
 
   return (
-    <div >
+    <div className='input-field-div'>
       <h3 className='input-title'>{title}</h3>
       <input className='input-field' placeholder={placeholder} style={{ border: "none" }} onClick={coloredLine} onPointerLeave={blackLine}></input>
       <div className='input-line'  ref={lineRef}></div>
