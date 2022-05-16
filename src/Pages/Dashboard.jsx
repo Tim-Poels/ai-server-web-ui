42d46cfd66337e8975042c1b2316cf9fdbf72e22
Tim-Poels <timpoels2000@gmail.com>
2022-05-16 11:58:23 +0200

    matrix with fade added to dashboard

--- src/Pages/Dashboard.jsx
+++ src/Pages/Dashboard.jsx
@@ -5,61 +5,75 @@ import { BiLogOut } from 'react-icons/bi'
 import NavbarButton from '../components/NavbarButton.jsx';
 import { Link, Outlet } from 'react-router-dom'
 import styled from 'styled-components';
+import { useEffect } from 'react';
+import Matrix from '../components/Matrix.js';
 
 
 
 const Dashboard = (props) => {
-    //The message is updated via props
-    const footerMessage = "Any issue? Contact your coach!"
-    //The username will be updated via Props
-    const userName = props.user
+	//The message is updated via props
+	const footerMessage = "Any issue? Contact your coach!";
+	//The username will be updated via Props
+	const userName = props.user;
+	useEffect(() => {
+		Matrix();
+	});
+	
+	const canvasStyle = {
+  	opacity: 0.5,
+	};
 
-  return (
-		<div className="body-container center">
-			<div className="dashboard-container">
-				<div className="main-container">
-					<div className="header">
-						{/* Fix this witout position absolute, does not work for smaller screens <1040 px*/}
-						<div className="logo-container">
-							<img className="logo" src={logo} alt="beCode logo"></img>
+		
+
+	return (
+		<div className="canvas-container">
+			<canvas id="canvasEl" style={canvasStyle}></canvas>
+			<div className="body-container center canvas-back">
+				<div className="dashboard-container">
+					<div className="main-container">
+						<div className="header">
+							{/* Fix this witout position absolute, does not work for smaller screens <1040 px*/}
+							<div className="logo-container">
+								<img className="logo" src={logo} alt="beCode logo"></img>
+							</div>
+							<div className="title">
+								<h2>Welcome, {userName}</h2>
+							</div>
+							<div className="log-out-container">
+								<Link to="/">
+									<BiLogOut />
+								</Link>
+							</div>
 						</div>
-						<div className="title">
-							<h2>Welcome, {userName}</h2>
-						</div>
-						<div className="log-out-container">
-						<Link to="/"> 
-							<BiLogOut />
-						</Link>
+						<div className="content-and-navbar-cont">
+							<div className="navbar">
+								<Adiv>
+									<Link to="launcher">
+										<NavbarButton text={"Training Launcher"} nmbr={1} />
+									</Link>
+									<Link to="queue">
+										<NavbarButton text={"Training Queue"} nmbr={2} />
+									</Link>
+									<Link to="past">
+										<NavbarButton text={"Past Trainings"} nmbr={3} />
+									</Link>
+								</Adiv>
+								<Bdiv>
+									<Cdiv>
+										<Ddiv className="tab1"></Ddiv>
+										<Ddiv className="tab2"></Ddiv>
+										<Ddiv className="tab3"></Ddiv>
+									</Cdiv>
+								</Bdiv>
+							</div>
+							<div className="content">
+								<Outlet />
+							</div>
 						</div>
 					</div>
-					<div className="content-and-navbar-cont">
-						<div className="navbar">
-							<Adiv>
-								<Link to="launcher">
-									<NavbarButton text={"Training Launcher"} nmbr={1} />
-								</Link>
-								<Link to="queue">
-									<NavbarButton text={"Training Queue"} nmbr={2} />
-								</Link>
-								<Link to="past">
-									<NavbarButton text={"Past Trainings"} nmbr={3} />
-								</Link>
-							</Adiv>
-							<Bdiv>
-								<Cdiv>
-									<Ddiv className="tab1"></Ddiv>
-									<Ddiv className="tab2"></Ddiv>
-									<Ddiv className="tab3"></Ddiv>
-								</Cdiv>
-							</Bdiv>
-						</div>
-						<div className="content">
-							<Outlet />
-						</div>
+					<div className="footer center">
+						<p>{footerMessage}</p>
 					</div>
-				</div>
-				<div className="footer center">
-					<p>{footerMessage}</p>
 				</div>
 			</div>
 		</div>
